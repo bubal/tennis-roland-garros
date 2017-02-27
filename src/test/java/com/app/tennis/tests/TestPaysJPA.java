dfaa5f588bd3faeee9421d4d30397822e917b6a9
bubu <bubu@riseup.net>
2017-02-27 23:33:04 +0100

    On ajoute les tests jdbc dans la classe test pour Pays

--- src/test/java/com/app/tennis/tests/TestPaysJPA.java
+++ src/test/java/com/app/tennis/tests/TestPaysJPA.java
@@ -18,24 +18,41 @@ import com.app.tennis.exceptions.DAOException;
 public class TestPaysJPA {
 	
 	private static DAOFactory daoFactory;
-	private static DAO<Pays> objDao;
+	private static DAO<Pays> objDaoJpa;
+	private static DAO<Pays> objDaoJdbc;
+	
 
 	@BeforeClass
-	public static void initDAO() throws DAOConfigurationException, DAOException{
+	public static void initDAO() throws DAOConfigurationException, DAOException, ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException{
 			daoFactory = DAOFactory.getInstance();
-			objDao = daoFactory.getObjJPA(Pays.class);
+			objDaoJpa = daoFactory.getObjJPA(Pays.class);
+			objDaoJdbc = daoFactory.getObjJDBC(Pays.class);
 		}
 	
 	@Test
-	public void testFindInt() throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, DAOException {
-		Pays obj = objDao.find(1);
+	public void testFindIntJpa() throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, DAOException {
+		Pays obj = objDaoJpa.find(1);
 		assertEquals("Angleterre",obj.getNom());
 	}
 
 	@Test
-	public void testListAll() throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, DAOException {
+	public void testListAllJpa() throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, DAOException {
 		List<Pays> listeObj = new ArrayList<Pays>();
-		listeObj = objDao.listAll();
+		listeObj = objDaoJpa.listAll();
+		assertTrue(listeObj.size()==17);
+		assertEquals("Angleterre",listeObj.get(1).getNom());
+	}
+	
+	@Test
+	public void testFindIntJdbc() throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, DAOException {
+		Pays obj = objDaoJdbc.find(1);
+		assertEquals("Angleterre",obj.getNom());
+	}
+
+	@Test
+	public void testListAllJdbc() throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, DAOException {
+		List<Pays> listeObj = new ArrayList<Pays>();
+		listeObj = objDaoJdbc.listAll();
 		assertTrue(listeObj.size()==17);
 		assertEquals("Angleterre",listeObj.get(1).getNom());
 	}
