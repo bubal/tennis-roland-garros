8769b7e93db4c306b0168ae23d202d060f5b7863
bubu <bubu@riseup.net>
2017-02-28 23:07:50 +0100

    On simplifie la Factory.

--- src/main/java/com/app/tennis/dao/DAOFactory.java
+++ src/main/java/com/app/tennis/dao/DAOFactory.java
@@ -1,13 +1,6 @@
 package com.app.tennis.dao;
 
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.ResourceBundle;
-
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
@@ -17,42 +10,9 @@ import com.app.tennis.exceptions.DAOException;
 
 public class DAOFactory {
 
-	private String url;
-	private String username;
-	private String password;
 	private static EntityManager entityManager;
 	
-	
-	DAOFactory( String url, String username, String password) {
-		this.url = url;
-		this.username = username;
-		this.password = password;
-	}
-
-	
-	
-	public static DAOFactory getInstance() throws DAOConfigurationException {
-
-		String driver; 
-		String url;
-		String username; 
-		String password;
-
-		ResourceBundle ressources = ResourceBundle.getBundle("paramDB") ; 
-
-		if (ressources == null){
-			throw new DAOConfigurationException( "Le fichier properties est introuvable." );
-		} 
-		try {
-			driver = ressources.getString("driver"); 
-			url = ressources.getString("url");
-			username = ressources.getString("username"); 
-			password = ressources.getString("password");
-
-			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
-			throw new DAOConfigurationException( "Le driver est introuvable.", e );
-		}
+	public DAOFactory() throws DAOConfigurationException{
 		
 		if (entityManager==null){
 			entityManager = Persistence.createEntityManagerFactory("tennispersistence").createEntityManager();
@@ -60,48 +20,11 @@ public class DAOFactory {
 				throw new DAOConfigurationException("Echec du chargement de la persistence");
 			}
 		}
-		DAOFactory instance = new DAOFactory( url, username, password);
-		return instance;
-	}
-
-	
-	public Connection getConnection() throws DAOException{
-		Connection connection = null;
-		try {
-			connection = DriverManager.getConnection(this.url,this.username,this.password);
-		} catch (SQLException e) {
-			throw new DAOException ("Echec de la connexion à la base de données !",e);
-		}
-		return connection;
-	}
-
-	
-	public EntityManager getEntityManager() throws DAOException{
-		return entityManager;
 	}
 	
-	
-	
-	/* Accès aux DAO jdbc */	
-	public <T> DAO<T> getObjJDBC(Class<T> typeClass) throws DAOException, ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException{
-		
-		String[] nameObj = typeClass.getName().split("\\.");
-		
-		String strClass = "com.app.tennis.dao.jdbc."+nameObj[nameObj.length-1]+"DAO";
-		
-		Class<?> classeDAO = Class.forName (strClass);
-		
-		@SuppressWarnings("unchecked")
-		DAO<T> objDAO = (DAO<T>) classeDAO.newInstance();
-		Method method = classeDAO.getMethod("setDaoFactory", DAOFactory.class);
-		method.invoke(objDAO, this);
-		return objDAO;
-	}
-	
-	
-	/* Accès au DAO jpa */
+	/* Accès aux DAO */
 	public <T> DAO<T> getObjJPA(Class<T> typeClass) throws DAOException{
-		return new ObjJPA<T>(this,typeClass);
+		return new ObjJPA<T>(entityManager,typeClass);
 	}
 	
 }
