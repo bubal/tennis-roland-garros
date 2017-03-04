e1c248e30ec18cc0711efc9ebc89854013ceb103
bubu <bubu@riseup.net>
2017-03-04 20:44:59 +0100

    La création de la base de données est désormais directement gérer par la DAOFactory

--- src/test/java/com/tests/tennis/dao/DAOTest.java
+++ src/test/java/com/tests/tennis/dao/DAOTest.java
@@ -1,16 +1,5 @@
 package com.tests.tennis.dao;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import org.h2.tools.RunScript;
-import org.hibernate.Session;
-import org.hibernate.jdbc.Work;
-import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 
 import com.app.tennis.dao.DAO;
@@ -29,54 +18,39 @@ import com.app.tennis.exceptions.DAOException;
 
 public class DAOTest {
 
-	private static String nameDB = "tennis-test-db";
+	private static String persistenceUnitName = "tennis-test-db";
+	private static String nameDB = "test-db";
+	
 	protected static DAOFactory daoFactory;
 
-	protected static DAO<Acces> accesDao;
-	protected static DAO<Pays> paysDao;
-	protected static DAO<Tournoi> tournoiDao;
-	protected static DAO<NiveauArbitre> niveauArbitreDao;
+	protected static DAO<Acces> 			accesDao;
+	protected static DAO<Pays>				paysDao;
+	protected static DAO<Tournoi>			tournoiDao;
+	protected static DAO<NiveauArbitre>		niveauArbitreDao;
 	protected static DAO<TypeQualification> typeQualificationDao;
-	protected static DAO<Joueur> joueurDao;
-	protected static DAO<Arbitre> arbitreDao;
-	protected static DAO<Court> courtDao;
-	protected static DAO<Match> matchDao;
+	protected static DAO<Joueur> 			joueurDao;
+	protected static DAO<Arbitre>			arbitreDao;
+	protected static DAO<Court>				courtDao;
+	protected static DAO<Match>				matchDao;
 
 	
 	@BeforeClass
 	public static void init() throws DAOConfigurationException, DAOException, Exception{
 
-		daoFactory=new DAOFactory(nameDB);
+		daoFactory = new DAOFactory(persistenceUnitName);
 
-		accesDao = daoFactory.getObjDAO(Acces.class);
-		paysDao = daoFactory.getObjDAO(Pays.class);
-		tournoiDao = daoFactory.getObjDAO(Tournoi.class);
-		niveauArbitreDao = daoFactory.getObjDAO(NiveauArbitre.class);
-		typeQualificationDao = daoFactory.getObjDAO(TypeQualification.class);
-		joueurDao = daoFactory.getObjDAO(Joueur.class);
-		arbitreDao = daoFactory.getObjDAO(Arbitre.class);
-		courtDao = daoFactory.getObjDAO(Court.class);
-		matchDao = daoFactory.getObjDAO(Match.class);
+		daoFactory.initDatabase( "/" + nameDB + ".sql" );
+		
+		accesDao 				=	daoFactory.getObjDAO(Acces.class);
+		paysDao 				=	daoFactory.getObjDAO(Pays.class);
+		tournoiDao 				=	daoFactory.getObjDAO(Tournoi.class);
+		niveauArbitreDao 		=	daoFactory.getObjDAO(NiveauArbitre.class);
+		typeQualificationDao 	=	daoFactory.getObjDAO(TypeQualification.class);
+		joueurDao 				=	daoFactory.getObjDAO(Joueur.class);
+		arbitreDao				=	daoFactory.getObjDAO(Arbitre.class);
+		courtDao 				=	daoFactory.getObjDAO(Court.class);
+		matchDao 				=	daoFactory.getObjDAO(Match.class);
 
-	}
-
-	
-	@Before
-	public void initializeDatabase(){
-
-		Session session = daoFactory.getEntityManager().unwrap(Session.class);
-		session.doWork(new Work() {
-			
-			@Override
-			public void execute(Connection connection) throws SQLException {
-				try {
-					File script = new File(getClass().getResource("/h2.sql").getFile());
-					RunScript.execute(connection, new FileReader(script));
-				} catch (FileNotFoundException e) {
-					throw new RuntimeException("could not initialize with script");
-				}
-			}
-		});
 	}
 
 	
