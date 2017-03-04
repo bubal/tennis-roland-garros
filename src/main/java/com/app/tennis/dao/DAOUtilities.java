cc6ac11a21f42c0bf6fee38693256c4738f5bee7
bubu <bubu@riseup.net>
2017-03-04 20:43:26 +0100

    On intègre la création de la base de données dans la class DAOUtilities

--- src/main/java/com/app/tennis/dao/DAOUtilities.java
+++ src/main/java/com/app/tennis/dao/DAOUtilities.java
@@ -1,8 +1,18 @@
 package com.app.tennis.dao;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import org.h2.tools.RunScript;
+import org.hibernate.Session;
+import org.hibernate.jdbc.Work;
 
 public class DAOUtilities {
 
@@ -15,11 +25,28 @@ public class DAOUtilities {
 	}
 	
 	public EntityManager getEntityManager(){
-		if (entityManager==null){
+		if (entityManager == null){
 			entityManagerFactory = Persistence.createEntityManagerFactory(this.persistenceUnitName);
 			entityManager = entityManagerFactory.createEntityManager();
 		}
 		return entityManager;
+	}
+	
+	public void initDatabase(String nameDB){
+
+		Session session = this.getEntityManager().unwrap(Session.class);
+		session.doWork(new Work() {
+			
+			@Override
+			public void execute(Connection connection) throws SQLException {
+				try {
+					File script = new File(getClass().getResource(nameDB).getFile());
+					RunScript.execute(connection, new FileReader(script));
+				} catch (FileNotFoundException e) {
+					throw new RuntimeException("could not initialize with script");
+				}
+			}
+		});
 	}
 	
 	public void closeEntityManager(){
