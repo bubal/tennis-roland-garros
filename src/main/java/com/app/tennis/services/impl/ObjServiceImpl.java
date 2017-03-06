dca14f65394997298d8244f6455a9876a4742374
bubu <bubu@riseup.net>
2017-03-06 22:05:02 +0100

    Ajout de la class générique ObjServiceImpl qui implémente les méthodes de l'interface générique ObjService.

---
+++ src/main/java/com/app/tennis/services/impl/ObjServiceImpl.java
@@ -0,0 +1,37 @@
+package com.app.tennis.services.impl;
+
+import java.util.List;
+
+import com.app.tennis.dao.DAO;
+import com.app.tennis.services.ObjService;
+
+public abstract class ObjServiceImpl<T> implements ObjService<T> {
+
+	abstract DAO<T> getDAO();
+	
+	@Override
+	public T create(T obj) {
+		return getDAO().create(obj);
+	}
+
+	@Override
+	public void deleteById(int id) {
+		getDAO().deleteById(id);
+	}
+
+	@Override
+	public T findById(int id) {
+		return getDAO().findById(id);
+	}
+
+	@Override
+	public T update(T obj) {
+		return getDAO().update(obj);
+	}
+
+	@Override
+	public List<T> listAll() {
+		return getDAO().listAll();
+	}
+
+}
