c0c976dc29159b82533c4b54a3744bb6290849e6
bubu <bubu@riseup.net>
2017-03-14 00:47:23 +0100

    On déporte la gestion de setExist() dans le service

--- src/main/java/com/app/tennis/services/AccesService.java
+++ src/main/java/com/app/tennis/services/AccesService.java
@@ -4,8 +4,8 @@ import com.app.tennis.data.Acces;
 
 public interface AccesService extends ObjService<Acces> {
 	
-	Acces findByLogin(String login);
-	
 	Acces grantedAcces(String login, String password);
+
+	Acces findByLogin(String string);
 
 }
