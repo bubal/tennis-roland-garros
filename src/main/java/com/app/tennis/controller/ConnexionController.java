f86853ead66532f56d0c01361aaa2076f741e68d
bubu <bubu@riseup.net>
2017-03-14 12:08:51 +0100

    Ajout du controller de connexion

---
+++ src/main/java/com/app/tennis/controller/ConnexionController.java
@@ -0,0 +1,11 @@
+package com.app.tennis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/connexion")
+public class ConnexionController {
+	
+
+}
