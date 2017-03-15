c5ace8e2978d333755c10779f5c2c2b690b1adbc
bubu <bubu@riseup.net>
2017-03-15 14:10:41 +0100

    Début du code pour le webservice joueur

---
+++ src/main/java/com/app/tennis/webservices/JoueurRest.java
@@ -0,0 +1,33 @@
+package com.app.tennis.webservices;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.tennis.data.Joueur;
+import com.app.tennis.services.JoueurService;
+
+@RestController
+@RequestMapping(value="/api/joueurs")
+public class JoueurRest {
+
+	@Autowired
+	JoueurService serviceJoueur;
+
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Joueur> getJoueurs(){
+		return serviceJoueur.listAll();
+	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public Joueur getJoueur(@PathVariable int id){
+		return serviceJoueur.findById(id);
+	}
+
+
+
+}
