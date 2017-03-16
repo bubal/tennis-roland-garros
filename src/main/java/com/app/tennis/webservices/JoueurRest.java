5f055b1eb6c216c0e8eec8fe76a69b60a2f5976d
bubu <bubu@riseup.net>
2017-03-16 16:11:00 +0100

    Ajout explicite du type de données produit

--- src/main/java/com/app/tennis/webservices/JoueurRest.java
+++ src/main/java/com/app/tennis/webservices/JoueurRest.java
@@ -3,6 +3,7 @@ package com.app.tennis.webservices;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,12 +19,12 @@ public class JoueurRest {
 	@Autowired
 	JoueurService serviceJoueur;
 
-	@RequestMapping(method=RequestMethod.GET)
+	@RequestMapping(method=RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
 	public List<Joueur> getJoueurs(){
 		return serviceJoueur.listAllFetchForRest();
 	}
 
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	@RequestMapping(value="/{id}", method=RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
 	public Joueur getJoueur(@PathVariable int id){
 		return serviceJoueur.findByIdFetchForRest(id);
 	}
