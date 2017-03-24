fc9a491c7effd618f351c3a8b90c05019a3d41c3
bubu <bubu@riseup.net>
2017-03-24 16:19:14 +0100

    Ajout d'un fichier de conf (commenté) pour les test rest

---
+++ src/test/java/com/app/tennis/tests/webservices/RestTestConfig.java
@@ -0,0 +1,17 @@
+//package com.app.tennis.tests.webservices;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.ImportResource;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//
+//import com.app.tennis.webservices.JoueurRest;
+//
+//
+//@Configuration
+//@EnableWebMvc
+//@ImportResource("classpath:spring-application-context.xml")
+//public class RestTestConfig {
+//	@Autowired
+//	JoueurRest serviceRest; 
+//}
