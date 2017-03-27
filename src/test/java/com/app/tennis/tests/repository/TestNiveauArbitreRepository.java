d5badafc9a79e81a75d7884d42e63a58e25ad764
bubu <bubu@riseup.net>
2017-03-27 12:16:59 +0200

    Ajout de l'annotation @WebAppConfiguration dans les classes de tests
    Sans cette annotation, le contexte des tests n'est pas chargé à cause de
    l'annotation @EnableWebMvc présent dans la config test des services
    REST.

--- src/test/java/com/app/tennis/tests/repository/TestNiveauArbitreRepository.java
+++ src/test/java/com/app/tennis/tests/repository/TestNiveauArbitreRepository.java
@@ -7,12 +7,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.app.tennis.data.NiveauArbitre;
 import com.app.tennis.services.NiveauArbitreService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring-application-context.xml"})
+@WebAppConfiguration
 public class TestNiveauArbitreRepository {
 	
 	@Autowired
--- src/test/java/com/app/tennis/tests/services/TestLogin.java
+++ src/test/java/com/app/tennis/tests/services/TestLogin.java
@@ -7,12 +7,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.app.tennis.data.Acces;
 import com.app.tennis.services.AccesService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring-application-context.xml"})
+@WebAppConfiguration
 public class TestLogin {
 	
 	@Autowired 
