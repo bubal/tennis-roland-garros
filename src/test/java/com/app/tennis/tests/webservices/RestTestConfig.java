65d79e4af4a5e03e1fbc294397ba57a1c4e46b25
bubu <bubu@riseup.net>
2017-03-27 12:14:37 +0200

    Ajout de la classe de config pour tester les services REST

--- src/test/java/com/app/tennis/tests/webservices/RestTestConfig.java
+++ src/test/java/com/app/tennis/tests/webservices/RestTestConfig.java
@@ -1,17 +1,19 @@
-//package com.app.tennis.tests.webservices;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.ImportResource;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//
-//import com.app.tennis.webservices.JoueurRest;
-//
-//
-//@Configuration
-//@EnableWebMvc
-//@ImportResource("classpath:spring-application-context.xml")
-//public class RestTestConfig {
-//	@Autowired
-//	JoueurRest serviceRest; 
-//}
+package com.app.tennis.tests.webservices;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.app.tennis.services.JoueurService;
+import com.app.tennis.services.impl.JoueurServiceImpl;
+
+
+@Configuration
+@EnableWebMvc
+@ImportResource("classpath:spring-application-context.xml")
+public class RestTestConfig {
+	public JoueurService joueurService() {
+		JoueurService joueurService = new JoueurServiceImpl();
+		return joueurService;
+	}
+}
