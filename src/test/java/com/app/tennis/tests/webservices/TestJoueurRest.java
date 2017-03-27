4c792675fb1d79ff818e922d30b86ed7bee3ec27
bubu <bubu@riseup.net>
2017-03-27 12:17:29 +0200

    Activation du test des services REST pour les joueurs

--- src/test/java/com/app/tennis/tests/webservices/TestJoueurRest.java
+++ src/test/java/com/app/tennis/tests/webservices/TestJoueurRest.java
@@ -1,65 +1,53 @@
-//package com.app.tennis.tests.webservices;
-//
-//import org.hamcrest.Matchers;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//import org.springframework.web.context.WebApplicationContext;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//
-//import com.app.tennis.webservices.JoueurRest;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = {"classpath:spring-application-context.xml"})
-//@WebAppConfiguration
-//public class TestJoueurRest {
-//
-//	@Autowired
-//	private WebApplicationContext webContext;
-//
-//	private MockMvc mockMvc;
-//
-//	@Before
-//	public void init() {
-//		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webContext).build();
-//	}
-//
-//	@Test
-//	public void checkGetJoueurByIdUrl() throws Exception{
-//		mockMvc.perform(MockMvcRequestBuilders.get("/api/joueurs/1"))
-//		.andExpect(MockMvcResultMatchers.status().isOk())
-//		.andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
-//		.andExpect(MockMvcResultMatchers.jsonPath("$.nom", Matchers.is("Murray")));
-//
-//	}
-//	@Test
-//	public void checkGetJoueurUrl() throws Exception{
-//		mockMvc.perform(MockMvcRequestBuilders.get("/api/joueurs"))
-//		.andExpect(MockMvcResultMatchers.status().isOk());
-//	}
-//
-//	@Test
-//	public void getJoueurs() throws Exception{
-//		mockMvc.perform(MockMvcRequestBuilders.get("/api/joueurs"))
-//		.andExpect(MockMvcResultMatchers.status().isOk())
-//		.andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(8)));
-//	}
-//
-//
-//	@Configuration
-//	@EnableWebMvc
-//	public static class TestConfiguration {
-//		@Autowired
-//		JoueurRest serviceRest; 
-//	}
-//
-//}
+package com.app.tennis.tests.webservices;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = RestTestConfig.class)
+@WebAppConfiguration
+public class TestJoueurRest {
+
+	@Autowired
+	private WebApplicationContext webContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void init() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webContext).build();
+	}
+
+	@Test
+	public void checkGetJoueurByIdUrl() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/joueurs/1"))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.nom", Matchers.is("Murray")));
+
+	}
+	@Test
+	public void checkGetJoueurUrl() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/joueurs"))
+		.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	public void getJoueurs() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/joueurs"))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(8)));
+	}
+
+}
